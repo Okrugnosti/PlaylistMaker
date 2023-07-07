be8ab2a65d8269c5ba0ef6f20efe77c721d58e7e
Okrugnosti <em2s.7.34@gmail.com>
2023-07-07 23:20:28 +0300

    #Styli 2

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,105 @@
 package com.example.playlistmaker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 
 class SearchActivity : AppCompatActivity() {
+    companion object {
+        const val PRODUCT_AMOUNT = "PRODUCT_AMOUNT"
+    }
+
+    private lateinit var editText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        val search_vector = findViewById<ImageView>(R.id.search_vector)
+        val editTextSearch = findViewById<EditText>(R.id.editTextSearch)
+        val clearButtonSearch = findViewById<ImageView>(R.id.clearButtonSearch)
+
+
+        //КНОПКА ВОЗВРАТА В ГЛАВНОЕ МЕНЮ
+        search_vector.setOnClickListener {
+            val displayIntent = Intent(this, MainActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+        //ОЧИСТКА ТЕКСТА В ПОЛЕ ВВОДА
+        clearButtonSearch.setOnClickListener {
+            editTextSearch.setText("")
+        }
+
+
+        val simpleTextWatcher = object : TextWatcher {
+
+            //заменя символов новым теĸстом
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            //событие используется, ĸогда нужно увидеть, ĸаĸие символы в теĸсте являются новыми
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+                /*
+                if (s.isNullOrEmpty()) {
+                    linearLayout.setBackgroundColor(getColor(R.color.prime_neutral))
+                } else {
+                    val input = s.toString()
+                    if (isPrime(input.toInt())) {
+                        linearLayout.setBackgroundColor(getColor(R.color.prime_positive))
+                    } else {
+                        linearLayout.setBackgroundColor(getColor(R.color.prime_negative))
+                    }
+                }
+                */
+
+                //включает функцию визуализации кнопки очистки
+                clearButtonSearch.visibility = clearButtonSearchVisibility(s)
+            }
+
+            //событие используется, когда нужно увидеть и, возможно, отредаĸтировать новый теĸст
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+
+        }
+
+        //устанавливает для объекта EditText созданый TextWatcher
+        editTextSearch.addTextChangedListener(simpleTextWatcher)
+        editText = findViewById<EditText?>(R.id.editTextSearch)
     }
+
+
+    // ЛОГИКА ВИЗУАЛИЗАЦИИ КНОПКИ ОЧИСТКИ ТЕКСТА
+    private fun clearButtonSearchVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val searchText = editText.text.toString()
+        outState.putString(PRODUCT_AMOUNT, searchText)
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val savedText = savedInstanceState.getString(PRODUCT_AMOUNT)
+        editText.setText(savedText)
+
+    }
+
 }
--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,21 +1,63 @@
 package com.example.playlistmaker
 
+import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
 class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
 
         val settingButton = findViewById<ImageView>(R.id.setting_button_back2)
+        val sendLinks = findViewById<ImageView>(R.id.sendLinks)
+        val mailSupport = findViewById<ImageView>(R.id.mailSupport)
+        val links = findViewById<ImageView>(R.id.links)
 
+
+        //ПОДЕЛИТЬСЯ ПРИЛОЖЕНИЕМ
+        sendLinks.setOnClickListener {
+            val data = "https://practicum.yandex.ru/android-developer/"
+            val openLinks = Intent(Intent.ACTION_SEND)
+            openLinks.putExtra(Intent.EXTRA_TEXT, data)
+            openLinks.setType("text/plain")
+            startActivity(openLinks)
+        }
+
+
+        //НАЗАД В ГЛАВНОЕ МЕНЮ
         settingButton.setOnClickListener {
             val displayIntent = Intent(this, MainActivity::class.java)
             startActivity(displayIntent)
         }
+
+
+        //ОТПРАВКА ПИСЬМА В ТЕХ.ПОДДЕРЖКУ
+        mailSupport.setOnClickListener {
+            //val mailAddress = Uri.parse("mail: mail@mail.ru")
+            val item = "Сообщение разработчикам и разработчицам приложения Playlist Maker"
+            val textMail = "Спасибо разработчикам и разработчицам за крутое приложение!"
+
+            val sendMail = Intent(Intent.ACTION_SENDTO)
+            //sendMail.putExtra(Intent.EXTRA_EMAIL, mailAddress)
+            sendMail.putExtra(Intent.EXTRA_SUBJECT, item)
+            sendMail.putExtra(Intent.EXTRA_TEXT, textMail)
+            sendMail.setData(Uri.parse("mailto:"))
+            startActivity(sendMail)
+        }
+
+
+        //ПЕРЕХОД НА ПОЛЬЗОВАТЕЛЬСКОЕ СОГЛАШЕНИЕ
+        links.setOnClickListener {
+            val address = Uri.parse("https://yandex.ru/legal/practicum_offer/")
+            val openLinks = Intent(Intent.ACTION_VIEW, address)
+            startActivity(openLinks)
+        }
+
     }
 }
